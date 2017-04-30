e80b888112716add458d5f683b08679b03b0ea86
shanadeb <sbeharry54@gmail.com>
2017-04-30 04:04:08 -0400

    User stuff
    Added logo, some dummy code for Player ID and a reportframe that appears
    after the user logs in.
    I kind of need some more direction with the reports

---
+++ src/UXUI/UXDungeonsandDynamites/src/reportframe.java
@@ -0,0 +1,58 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.net.URL;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+public class reportframe extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					reportframe frame = new reportframe();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public reportframe() {
+		URL iconURL = getClass().getResource("Dynamite.png");
+		ImageIcon icon= new ImageIcon(iconURL);
+		setIconImage(icon.getImage());
+		setTitle("Dungeons And Dynamites");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("New menu item");
+		mnFile.add(mntmNewMenuItem);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
